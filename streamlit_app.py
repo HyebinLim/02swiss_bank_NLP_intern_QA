4594c44e2313e52b28be9e436ac710e89b3d2b01
HyebinLim <imyourbest0912@gmail.com>
2025-07-06 15:26:40 +0900

    simplify description

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,9 @@ st.set_page_config(
 # --- 앱 설명 표시 ---
 st.header("🏦Want a Swiss Bank AI/NLP job?")
 st.markdown("""
-Ask me any questions about my full journey for an AI/NLP scientist position at a bank in Zurich, 🇨🇭 during my master's at University of Zurich. Source: [(89% 직장인 일지)](https://blog.naver.com/imyourbest)
+Ask me anything about my journey to become an AI/NLP scientist at a Swiss bank in Zurich during my master's at University of Zurich.
+
+Source: [89% 직장인 일지](https://blog.naver.com/imyourbest)
 """)
 
 # --- API 키 입력 ---
