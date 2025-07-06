c93ee4bea68e0d63058d8b236d57b5d111b4986e
HyebinLim <imyourbest0912@gmail.com>
2025-07-06 17:17:29 +0900

    adjust font size

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,8 +63,8 @@ h2 {
     border-radius: 8px;
     margin: 15px 0;
     font-family: 'Poppins', sans-serif;
-    font-size: 14px;
-    font-weight: 500;
+    font-size: 16px;
+    font-weight: 600;
     color: #2c3e50;
     text-align: center;
 }
